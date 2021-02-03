8ee0c89faa3e134556d6798d0c3123bab442de7f
Mike Marino <marino1820@gmail.com>
2021-02-03 18:47:22 -0500

    Menu updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
  $(".dropdown-trigger").dropdown({
      coverTrigger: false,
      closeOnClick: false,
-      inDuration: 300,
-          outDuration: 225,
-          constrainWidth: false, // Does not change width of dropdown to that of the activator
+    //   inDuration: 300,
+        //   outDuration: 225,
+        //   constrainWidth: false, // Does not change width of dropdown to that of the activator
           hover: true, // Activate on hover
           gutter: 0, // Spacing from edge
           belowOrigin: false, // Displays dropdown below the button
-          alignment: 'left', // Displays dropdown with edge aligned to the left of button
-          stopPropagation: false // Stops event propagation
+        //   alignment: 'right', // Displays dropdown with edge aligned to the left of button
+        //   stopPropagation: false // Stops event propagation
  });
 
 $(document).ready(function () {
