99ad0b1b9832f649110f9ebf870f9cc59c50456a
Gary B <grider27@gmail.com>
2021-02-04 18:21:53 -0500

    news feed and clean up html and css for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,16 +55,38 @@ $(document).ready(function () {
                 nflDiv.append(teamBtn)
 
                 //attach  nflDiv to li-nfl
-                $(".nflTeam").append(nflDiv)
-
-
-
-
+                $("#NFL").append(nflDiv)
             }
         })
 
     }
 
 nflDisplay();
+
+
+
+// gary adding code for news feed
+var teamName = "philadelphia eagles" // need to get name of team from above code when ready! ****
+var requestNewUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + teamName + "&api-key=3XNPIhbw16I5OElGvNEztFieigRzON44"
+fetch(requestNewUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (detail) {
+        var getArticles = detail.response.docs;
+        let outputNews = `<h4>Latest News:</h4>`;         
+        $.each(getArticles, function (i, val) {
+            if (i > 3) return;
+            outputNews += `<p>${this.headline.main}</p>
+            <a target="_blank" href="${this.web_url}">
+            <i class="tiny material-icons">launch</i></a>
+            </p>
+            `;
+        })
+        $('#newsFeed').append(outputNews);
+    })
+
+
+
 });
 
