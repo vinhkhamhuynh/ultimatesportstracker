9f679468b58abedc2087543e607fd8f4c27c434b
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-02-06 13:32:49 -0500

    completed NFL dropdown with team name and logo. fix class mega width to 625px

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
- $(".dropdown-trigger").dropdown({
-     coverTrigger: false,
-     closeOnClick: false,
+$(".dropdown-trigger").dropdown({
+    coverTrigger: false,
+    closeOnClick: false,
     //   inDuration: 300,
-        //   outDuration: 225,
-        //   constrainWidth: false, // Does not change width of dropdown to that of the activator
-          hover: true, // Activate on hover
-          gutter: 0, // Spacing from edge
-          belowOrigin: false, // Displays dropdown below the button
-        //   alignment: 'right', // Displays dropdown with edge aligned to the left of button
-        //   stopPropagation: false // Stops event propagation
- });
+    //   outDuration: 225,
+    //   constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    //   alignment: 'right', // Displays dropdown with edge aligned to the left of button
+    //   stopPropagation: false // Stops event propagation
+});
 
 $(document).ready(function () {
 
@@ -19,80 +19,336 @@ $(document).ready(function () {
         accordion: false
     });
 
-     //vh get api to display nfl team when user hover over "nfl" on nav bar
-     function nflDisplay() {
+    //vh get api to display NFL team when user hover over "NFL" on nav bar
+    function nflDisplay() {
 
         //vh create var from api link 
         var requestNflUrl = "https://api.sportsdata.io/v3/nfl/scores/json/AllTeams?key=c92709fa7d9a4261bf1ee17785767b5b";
-      
+
         //vh fetch data to use
         fetch(requestNflUrl)
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(nflData){
-            console.log(nflData);
-
-            
-            //vh loop data to generate all team info
-            for (var i = 0 ; i< nflData.length; i++) {
-                //vh03
-                // if (i === 0 || i === 25 || i === 36 ) break;
-                //vh create a div to hold team name btn and img with class to check img size in css 
-                // var nflDiv = $("<div>").attr("id", "nflDiv")
-
-                //vh03
-                var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (nflData) {
+                console.log(nflData);
 
 
-                //vh generate img url from api
-                var imgUrl = nflData[i].WikipediaLogoUrl
+                //vh loop data to generate all team info group 1
+                for (var i = 1; i < 9; i++) {
+                    //vh03
+                    // if (i === 0 || i === 25 || i === 36 ) break;
+                    //vh create a div to hold team name btn and img with class to check img size in css 
+                    // var nflDiv = $("<div>").attr("id", "nflDiv")
 
-                //vh create img tag to hold url 
-                var teamImg= $("<img>").attr({"id": "teamImg", "src": imgUrl}).addClass("circle")
+                    //vh03
+                    var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
 
-                //vh create team btn and attach name to button 
-                var teamSpan =$("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
 
-                //vh attach img to nflDiv
-                nflDiv.append(teamImg)
-                //vh attach btn to nflDiv
-                nflDiv.append(teamSpan)
+                    //vh generate img url from api
+                    var imgUrl = nflData[i].WikipediaLogoUrl
 
-                //vh attach nflDiv to li-nfl
-                // $("#NFL").append(nflDiv)
+                    //vh create img tag to hold url 
+                    var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
 
-                //vh03
-                $(".collection").append(nflDiv)
-            }
-        })
+                    //vh create team btn and attach name to button 
+                    var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+                    //vh attach img to nflDiv
+                    nflDiv.append(teamImg)
+                    //vh attach btn to nflDiv
+                    nflDiv.append(teamSpan)
+
+                    //vh attach nflDiv to li-nfl
+                    // $("#NFL").append(nflDiv)
+
+                    //vh03
+                    $(".group1").append(nflDiv)
+
+
+                }
+
+                // vh020621 group 2
+                for (var i = 9; i < 17; i++) {
+                    //vh03
+                    // if (i === 0 || i === 25 || i === 36 ) break;
+                    //vh create a div to hold team name btn and img with class to check img size in css 
+                    // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+                    //vh03
+                    var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+                    //vh generate img url from api
+                    var imgUrl = nflData[i].WikipediaLogoUrl
+
+                    //vh create img tag to hold url 
+                    var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+                    //vh create team btn and attach name to button 
+                    var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+                    //vh attach img to nflDiv
+                    nflDiv.append(teamImg)
+                    //vh attach btn to nflDiv
+                    nflDiv.append(teamSpan)
+
+                    //vh attach nflDiv to li-nfl
+                    // $("#NFL").append(nflDiv)
+
+                    //vh03
+                    $(".group2").append(nflDiv)
+                }
+
+                // vh020621 group 3
+                for (var i = 17; i < 28; i++) {
+                    //vh03
+                    if (i === 19 || i === 20 || i === 25) { continue };
+                    //vh create a div to hold team name btn and img with class to check img size in css 
+                    // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+                    //vh03
+                    var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+                    //vh generate img url from api
+                    var imgUrl = nflData[i].WikipediaLogoUrl
+
+                    //vh create img tag to hold url 
+                    var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+                    //vh create team btn and attach name to button 
+                    var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+                    //vh attach img to nflDiv
+                    nflDiv.append(teamImg)
+                    //vh attach btn to nflDiv
+                    nflDiv.append(teamSpan)
+
+                    //vh attach nflDiv to li-nfl
+                    // $("#NFL").append(nflDiv)
+
+                    //vh03
+                    $(".group3").append(nflDiv)
+                }
+
+                // vh020621 group 4
+                for (var i = 28; i < 37; i++) {
+                    //vh03
+                    if (i === 30) { continue; }
+                    //vh create a div to hold team name btn and img with class to check img size in css 
+                    // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+                    //vh03
+                    var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+                    //vh generate img url from api
+                    var imgUrl = nflData[i].WikipediaLogoUrl
+
+                    //vh create img tag to hold url 
+                    var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+                    //vh create team btn and attach name to button 
+                    var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+                    //vh attach img to nflDiv
+                    nflDiv.append(teamImg)
+                    //vh attach btn to nflDiv
+                    nflDiv.append(teamSpan)
+
+                    //vh attach nflDiv to li-nfl
+                    // $("#NFL").append(nflDiv)
+
+                    //vh03
+                    $(".group4").append(nflDiv)
+                }
+            })
+
+
 
     }
 
-nflDisplay();
+    nflDisplay();
+
+    // vh020621 get api to display NBA team when user hover over NBA on navbar 
+
+//    function nbaDisplay() {
+
+//         //vh create var from api link 
+//         var requestNflUrl = "https://api.sportsdata.io/v3/nba/scores/json/AllTeams?key=ae840c1a34fe4e84bd31aefb540743c6";
+
+//         //vh fetch data to use
+//         fetch(requestNflUrl)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (nflData) {
+//                 console.log(nflData);
+
+
+//                 //vh loop data to generate all team info group 1
+//                 for (var i = 1; i < 9; i++) {
+//                     //vh03
+//                     // if (i === 0 || i === 25 || i === 36 ) break;
+//                     //vh create a div to hold team name btn and img with class to check img size in css 
+//                     // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+//                     //vh03
+//                     var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+//                     //vh generate img url from api
+//                     var imgUrl = nflData[i].WikipediaLogoUrl
+
+//                     //vh create img tag to hold url 
+//                     var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+//                     //vh create team btn and attach name to button 
+//                     var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+//                     //vh attach img to nflDiv
+//                     nflDiv.append(teamImg)
+//                     //vh attach btn to nflDiv
+//                     nflDiv.append(teamSpan)
+
+//                     //vh attach nflDiv to li-nfl
+//                     // $("#NFL").append(nflDiv)
+
+//                     //vh03
+//                     $(".group1").append(nflDiv)
+
+
+//                 }
+
+//                 // vh020621 group 2
+//                 for (var i = 9; i < 17; i++) {
+//                     //vh03
+//                     // if (i === 0 || i === 25 || i === 36 ) break;
+//                     //vh create a div to hold team name btn and img with class to check img size in css 
+//                     // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+//                     //vh03
+//                     var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+//                     //vh generate img url from api
+//                     var imgUrl = nflData[i].WikipediaLogoUrl
+
+//                     //vh create img tag to hold url 
+//                     var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+//                     //vh create team btn and attach name to button 
+//                     var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+//                     //vh attach img to nflDiv
+//                     nflDiv.append(teamImg)
+//                     //vh attach btn to nflDiv
+//                     nflDiv.append(teamSpan)
+
+//                     //vh attach nflDiv to li-nfl
+//                     // $("#NFL").append(nflDiv)
+
+//                     //vh03
+//                     $(".group2").append(nflDiv)
+//                 }
+
+//                 // vh020621 group 3
+//                 for (var i = 17; i < 28; i++) {
+//                     //vh03
+//                     if (i === 19 || i === 20 || i === 25) { continue };
+//                     //vh create a div to hold team name btn and img with class to check img size in css 
+//                     // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+//                     //vh03
+//                     var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+//                     //vh generate img url from api
+//                     var imgUrl = nflData[i].WikipediaLogoUrl
+
+//                     //vh create img tag to hold url 
+//                     var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+//                     //vh create team btn and attach name to button 
+//                     var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+//                     //vh attach img to nflDiv
+//                     nflDiv.append(teamImg)
+//                     //vh attach btn to nflDiv
+//                     nflDiv.append(teamSpan)
+
+//                     //vh attach nflDiv to li-nfl
+//                     // $("#NFL").append(nflDiv)
+
+//                     //vh03
+//                     $(".group3").append(nflDiv)
+//                 }
+
+//                 // vh020621 group 4
+//                 for (var i = 28; i < 37; i++) {
+//                     //vh03
+//                     if (i === 30) { continue; }
+//                     //vh create a div to hold team name btn and img with class to check img size in css 
+//                     // var nflDiv = $("<div>").attr("id", "nflDiv")
+
+//                     //vh03
+//                     var nflDiv = $("<li>").addClass("collection-item avatar").attr("id", "nflItem")
+
+
+//                     //vh generate img url from api
+//                     var imgUrl = nflData[i].WikipediaLogoUrl
+
+//                     //vh create img tag to hold url 
+//                     var teamImg = $("<img>").attr({ "id": "teamImg", "src": imgUrl }).addClass("circle")
+
+//                     //vh create team btn and attach name to button 
+//                     var teamSpan = $("<span>").attr("TeamID", nflData[i].TeamID).addClass("title").text(nflData[i].Name)
+
+//                     //vh attach img to nflDiv
+//                     nflDiv.append(teamImg)
+//                     //vh attach btn to nflDiv
+//                     nflDiv.append(teamSpan)
+
+//                     //vh attach nflDiv to li-nfl
+//                     // $("#NFL").append(nflDiv)
+
+//                     //vh03
+//                     $(".group4").append(nflDiv)
+//                 }
+//             })
 
 
 
-// gary adding code for news feed
-var teamName = "philadelphia eagles" // need to get name of team from above code when ready! ****
-var requestNewUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + teamName + "&api-key=3XNPIhbw16I5OElGvNEztFieigRzON44"
-fetch(requestNewUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (detail) {
-        var getArticles = detail.response.docs;
-        let outputNews = `<h4>Latest News:</h4>`;         
-        $.each(getArticles, function (i, val) {
-            if (i > 3) return;
-            outputNews += `<p>${this.headline.main}</p>
+//     }
+
+//     nbaDisplay();
+
+
+
+
+
+
+    // gary adding code for news feed
+    var teamName = "philadelphia eagles" // need to get name of team from above code when ready! ****
+    var requestNewUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + teamName + "&api-key=3XNPIhbw16I5OElGvNEztFieigRzON44"
+    fetch(requestNewUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (detail) {
+            var getArticles = detail.response.docs;
+            let outputNews = `<h4>Latest News:</h4>`;
+            $.each(getArticles, function (i, val) {
+                if (i > 3) return;
+                outputNews += `<p>${this.headline.main}</p>
             <a target="_blank" href="${this.web_url}">
             <i class="tiny material-icons">launch</i></a>
             </p>
             `;
+            })
+            $('#newsFeed').append(outputNews);
         })
-        $('#newsFeed').append(outputNews);
-    })
 
 
 
